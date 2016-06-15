0467eeecedfb20d6a73b7e7fd067a568b531f5e4
root <root@vps213149.ovh.net>
2016-06-15 05:27:55 +0200

    potions removed from first items && mwall mlvl decrease for knights

--- war_config.lua
+++ war_config.lua
@@ -37,8 +37,7 @@ commonItems = {
 
 backpackItems = {
   { 				---------SORCERER ITEMS---------							     
-	{itemid=7590, count=100},  -- great mana potion
-	{itemid=7590, count=100},  -- great mana potion
+	{itemid=7590, count=1},  -- great mana potion
 	{itemid=2274, count=1},  -- AVA
 	{itemid=2268, count=1},  -- SD
 	{itemid=2273, count=1},  -- UH
@@ -48,36 +47,31 @@ backpackItems = {
 	{itemid=2789, count=1},  -- BROWN MUSHROOM
   },
   { 				---------DRUID ITEMS---------
-	{itemid=7590, count=100},   -- great mana potion
-	{itemid=7590, count=100},  -- great mana potion
-	{itemid=2274, count=1},     -- AVA
-	{itemid=2268, count=1},     -- SD
-	{itemid=2273, count=1},     -- UH
-	{itemid=2293, count=1},     -- MW
-	{itemid=2305, count=1},     -- FB
-	{itemid=2261, count=1},     -- DESTROY
-	{itemid=2278, count=1},     -- PARALYZE RUNE	
-	{itemid=2789, count=1},     -- BROWN MUSHROOM
+	{itemid=7590, count=1},   -- great mana potion
+	{itemid=2274, count=1},   -- AVA
+	{itemid=2268, count=1},   -- SD
+	{itemid=2273, count=1},   -- UH
+	{itemid=2293, count=1},   -- MW
+	{itemid=2305, count=1},   -- FB
+	{itemid=2261, count=1},   -- DESTROY
+	{itemid=2278, count=1},   -- PARALYZE RUNE	
+	{itemid=2789, count=1},   -- BROWN MUSHROOM
   },
   { 				---------PALADIN ITEMS---------
-	{itemid=8472, count=100},  -- spirit
-	{itemid=8472, count=100},  -- spirit
-	{itemid=8472, count=100},  -- spirit
-	{itemid=2305, count=1},   -- FB
-	{itemid=2261, count=1}, -- DESTROY
-	{itemid=2293, count=1},    -- MW
-	{itemid=2789, count=1},    -- BROWN MUSHROOM
+	{itemid=8472, count=1},  -- spirit
+	{itemid=2305, count=1},  -- FB
+	{itemid=2261, count=1},  -- DESTROY
+	{itemid=2293, count=1},  -- MW
+	{itemid=2789, count=1},  -- BROWN MUSHROOM
   },
   { 				---------KNIGHT ITEMS---------
-	{itemid=7402, count=1}, -- giant sword
-	{itemid=7591, count=100},  -- great health potion
-	{itemid=7591, count=100},  -- great health potion
-	{itemid=7620, count=100}, -- mana potion
-	{itemid=7620, count=100}, -- mana potion
-	{itemid=2293, count=1},     -- MW
-	{itemid=2261, count=1}, -- DESTROY
-	{itemid=2305, count=1}, -- FB
-	{itemid=2789, count=1},    -- BROWN MUSHROOM
+	{itemid=7402, count=1},  -- giant sword
+	{itemid=7591, count=1},  -- great health potion
+	{itemid=7620, count=1},  -- mana potion
+	{itemid=2293, count=1},  -- MW
+	{itemid=2261, count=1},  -- DESTROY
+	{itemid=2305, count=1},  -- FB
+	{itemid=2789, count=1},  -- BROWN MUSHROOM
   }
 }
 
